fec95960848fbf81e13939d90188149a94baf089
Sikarin Poonsawat <afumagic3@gmail.com>
2017-11-05 12:30:36 +0700

    set heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+var port = process.env.PORT || 3000 ;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
@@ -51,4 +52,5 @@ app.get('/bad', (req, res) => {
     });
 })
 
-app.listen(3000);
+app.listen(port);
+console.log("Port is "+port);
